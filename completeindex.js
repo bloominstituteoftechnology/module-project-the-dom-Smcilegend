cfdd4ff225dcb848c500a3c571366d5f568c91f9
Michael Gibson <96648835+Smcilegend@users.noreply.github.com>
2024-06-29 10:36:48 -0700

    Add files via upload

---
+++ completeindex.js
@@ -0,0 +1,102 @@
+function moduleProject1() {
+  const footer = document.querySelector('footer')
+  const currentYear = new Date().getFullYear()
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
+
+  // 👇 WORK WORK BELOW THIS LINE 👇
+
+  // 👉 TASK 1 - Add a "widget" class name to widgets so CSS kicks in
+  // ✨ add your code here
+    const widgets = document.querySelectorAll("section>div");
+    widgets.forEach(widget => {
+      widget.classList.add('widget');
+    })
+
+  // 👉 TASK 2 - Build a "Quote of the Day" widget
+  //  ✨ add your code here
+    const randomIdx = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIdx];
+
+    //flesh out quote
+    const quote = document.createElement('div');
+    const quoteText = randomQuote.quote;
+    quote.textContent = quoteText;
+    const widget1 = document.querySelector('.quoteoftheday');
+    widget1.appendChild(quote);
+
+    //flash out author & date
+    const authorDate = document.createElement('div');
+    const {author, date} = randomQuote;
+    authorDate.textContent = `${author} in ${date} ` || "an unknown date";
+    widget1.appendChild(authorDate);
+    console.log(authorDate);
+    
+  // 👉 TASK 3 - Build a "Corporate Speak" widget
+  //  ✨ add your code here
+    randomVerb1 = Math.floor(Math.random() * verbs.length);
+    randomVerb1 = Math.floor(Math.random() * verbs.length);
+
+    console.log(randomVerb1)
+
+
+  // 👉 TASK 4 - Build a "Countdown" widget
+  //  ✨ add your code here
+  const countDownWidget =document.querySelector(.countdown);
+  let count = 5
+  const countdown = document.createElement("p");
+  countdown.textContent = `T-minus ${count}...`
+  countdownWidget.appendChild(countdown)
+
+  const id = setInterval(() => {
+    if (count === 1) {
+      countdown.textContent = 'Liftoff! 🚀'
+      clearInterval(id)
+    }else {
+
+      countdown.textContent = `T-minus ${--count}...`
+    }
+  }, 1000)
+    
+
+
+  // 👉 TASK 5 - Build a "Friends" widget
+  //  ✨ add your code here
+  const person = people[Math.floor(Math.random() * people.length)]
+  const personParagraph = document.createElement("p")
+  document.querySelector('.friends').appendChild(personParagraph)
+  const year = person.dateOfBirth.split('-')[0]
+  let sentence = `${person.fname} ${person.lname} was born in ${year} and ${}`
+
+  if (!person.friends.length) {
+    sentence += 'has no friends'
+  }else {
+    sentence += 'is friends with others'
+    for (let idx = 0; idx < person.friends.length; idx++) {
+      const friendId = person.friends[idx]
+      const friend = people.find(p => p.id === friendId)
+      const fullName = `${friend.lname}`
+      let isLastidx = idx === person.friends.length - 1
+      let isNextToLastIdx = idx === person.friends.length - 2
+      if (isLastIdx) {
+        sentence += `${fullName}.`
+      }else if (isNextToLastIdx) {
+        sentence += `{fullName} and `
+      } else {
+        sentence += `${fullName}`
+      }
+      }
+      }
+  personParagraph.textContent = sentence
+  console.log(person)
+
+  // 👉 TASK 6 - Make it so user can tab through the widgets
+  //  ✨ add your code here
+
+  // 👆 WORK WORK ABOVE THIS LINE 👆
+}
+
+// ❗ DO NOT WORK BELOW THIS LINE
+// ❗ DO NOT WORK BELOW THIS LINE
+// ❗ DO NOT WORK BELOW THIS LINE
+if (typeof module !== 'undefined' && module.exports) module.exports = { moduleProject1 }
+else moduleProject1()
